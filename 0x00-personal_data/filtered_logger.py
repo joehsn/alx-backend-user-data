1390d752125a8d2c90126783e87fb970b80dc954
joehsn <joehsn@outlook.com>
2024-11-07 22:35:15 +0000

    tasks 1, 2 and 3 of 0x00 personal data project corrected

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,56 +1,35 @@
 #!/usr/bin/env python3
+
 """
 0. Regex-ing task's module.
 """
 
-import re
 import logging
-from typing import List, Tuple
-from datetime import datetime
-
-
-patterns = {
-    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-    'replace': lambda x: r'\g<field>={}'.format(x),
-}
+import re
+from typing import List
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(
-    fields: List[str], redaction: str, message: str, separator: str,
+    fields: List[str],
+    redaction: str,
+    message: str,
+    separator: str
 ) -> str:
-    """
-    Filters a log line.
-    """
-    extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, separator), replace(redaction), message)
-
-
-def asc_time() -> str:
-    """
-    Returns the current time.
-    """
-    cur_time = datetime.now()
-    cur_time_ms = cur_time.microsecond // 1000
-    return str('{},{}'.format(cur_time.strftime("%F %X"), cur_time_ms))
-
-
-def get_values(record: logging.LogRecord, msg: str) -> Tuple[str]:
-    """
-    Retrieves values to be printed for a log record.
-    """
-    asctime = asc_time()
-    return (record.name, record.levelname, asctime, msg.replace(';', '; '))
+    pattern = f"({'|'.join(fields)})=[^;]*"
+    return re.sub(
+        pattern,
+        lambda match: f"{match.group(1)}={redaction}",
+        message
+    )
 
 
 class RedactingFormatter(logging.Formatter):
-    """
-    Redacting Formatter class
-    """
+    """ Redacting Formatter class  """""
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
@@ -58,22 +37,22 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """
-        formats a LogRecord.
-        """
-        tmp = record.getMessage()
-        msg = filter_datum(self.fields, self.REDACTION, tmp, self.SEPARATOR)
-        values = get_values(record, msg)
-        return self.FORMAT % dict(zip(self.FORMAT_FIELDS, values))
+        record.msg = filter_datum(
+            self.fields,
+            self.REDACTION,
+            record.getMessage(),
+            self.SEPARATOR
+        )
+        return super().format(record)
 
+    def get_logger() -> logging.Logger:
+        """Creates and configures a logger."""""
+        logger = logging.getLogger("user_data")
+        logger.setLevel(logging.INFO)
+        logger.propagate = False
 
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
 
-def get_logger() -> logging.Logger:
-    """Creates a new logger for user data.
-    """""
-    logger = logging.Logger("user_data", logging.INFO)
-    stream_handler = logging.StreamHandler()
-    stream_handler.formatter = RedactingFormatter(PII_FIELDS)
-    logger.addHandler(stream_handler)
-    logger.propagate = False
-    return logger
+        logger.addHandler(stream_handler)
+        return logger
