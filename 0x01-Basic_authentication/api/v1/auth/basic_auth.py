ed0ac6fcaff639cdcb3b5a18dd6b0e9f79f8bb1a
joehsn <joehsn@outlook.com>
2024-11-12 18:54:08 +0000

    9. Basic - User credentials task is done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ import re
 import base64
 import binascii
 
+from typing import Tuple
 from .auth import Auth
 
 
@@ -43,18 +44,22 @@ class BasicAuth(Auth):
             except binascii.Error:
                 return None
 
-    def decode_base64_authorization_header(
-        self,
-        base64_authorization_header: str,
-    ) -> str:
-        """Decodes a base64-encoded authorization header.
-        """
-        if type(base64_authorization_header) == str:
-            try:
-                res = base64.b64decode(
-                    base64_authorization_header,
-                    validate=True,
+        def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str,
+        ) -> Tuple[str, str]:
+            """Extracts user credentials from a base64-decoded authorization
+            header that uses the Basic authentication flow.
+            """
+            if type(decoded_base64_authorization_header) == str:
+                pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+                field_match = re.fullmatch(
+                    pattern,
+                    decoded_base64_authorization_header.strip(),
+
                 )
-                return res.decode('utf-8')
-            except binascii.Error:
-                return None
+                if field_match is not None:
+                    user = field_match.group('user')
+                    password = field_match.group('password')
+                    return user, password
+            return None, None
