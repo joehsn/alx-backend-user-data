f7e46808985338c6f6e083b66feb0068c78636b1
joehsn <joehsn@outlook.com>
2024-11-12 18:44:47 +0000

    7. Basic - Base64 part task is done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,44 @@
 #!/usr/bin/env python3
 """Basic authentication module for the API.
 """
+import re
+import base64
+import binascii
+
 from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Basic authentication class.
     """
-    pass
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
+        """Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.groups('token')[0]
+            return None
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str,
+
+    ) -> str:
+        """Decodes a base64-encoded authorization header.
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+
+                )
+                return res.decode('utf-8')
+            except binascii.Error:
+                return None
