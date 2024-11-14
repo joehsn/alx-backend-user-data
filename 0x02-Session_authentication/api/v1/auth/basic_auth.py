4be526d71a266bfd96fa754f503268a38016a377
joehsn <joehsn@outlook.com>
2024-11-14 18:35:40 +0000

    0. Et moi et moi et moi! task is done

---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""Basic authentication module for the API.
+"""
+import re
+import base64
+import binascii
+
+from typing import Tuple
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class.
+    """
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
+        """Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.groups('token')[0]
+            return None
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str,
+
+    ) -> str:
+        """Decodes a base64-encoded authorization header.
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+
+                )
+                return res.decode('utf-8')
+            except binascii.Error:
+                return None
+
+        def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str,
+        ) -> Tuple[str, str]:
+            """Extracts user credentials from a base64-decoded authorization
+            header that uses the Basic authentication flow.
+            """
+            if type(decoded_base64_authorization_header) == str:
+                pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+                field_match = re.fullmatch(
+                    pattern,
+                    decoded_base64_authorization_header.strip(),
+
+                )
+                if field_match is not None:
+                    user = field_match.group('user')
+                    password = field_match.group('password')
+                    return user, password
+            return None, None
