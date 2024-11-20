900c659add938fac5ea2248a4d68315bc189934b
joehsn <joehsn@outlook.com>
2024-11-20 18:43:51 +0000

    5. Register user task: done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 """Auth module.
 """
+from db import DB
+from user import User
+from typing import Optional
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
-def _hash_password(password: str) -> bytes:
-    """Hashes a password using bcrypt.
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """Hashes a password using bcrypt.
+        """
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user with an email and password.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError()
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            user = self._db.add_user(email, hashed_password.decode('utf-8'))
+            return user
