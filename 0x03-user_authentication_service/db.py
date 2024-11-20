731e721f578108120430cf8be0f3286a20a9d395
joehsn <joehsn@outlook.com>
2024-11-20 18:11:54 +0000

    3. update user task: corrected

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,25 +46,16 @@ class DB:
         try:
             return self._session.query(User).filter_by(**kwargs).one()
         except NoResultFound:
-            raise NoResultFound("No user found with the specified criteria.")
+            raise NoResultFound()
         except InvalidRequestError:
-            raise InvalidRequestError("Invalid query arguments provided.")
+            raise InvalidRequestError()
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """Updates a user based on a given id.
         """
         user = self.find_user_by(id=user_id)
-        if user is None:
-            return
-        update_source = {}
         for key, value in kwargs.items():
-            if hasattr(User, key):
-                update_source[getattr(User, key)] = value
-            else:
+            if not hasattr(user, key):
                 raise ValueError()
-        self._session.query(User).filter(User.id == user_id).update(
-            update_source,
-            synchronize_session=False,
-
-        )
+            setattr(user, key, value)
         self._session.commit()
